d9e2843cc3b75a5661afcf4be1ad97d9c16c098e
Sam Winkles <swinkles92@live.com>
2023-07-20 15:32:04 -0600

    Created Locations webpage files
    Locations files will store information regarding the SWC Bank branches and their locations.

---
+++ Bank.Web/Pages/Locations.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Bank.Web.Pages
+{
+	public class LocationsModel : PageModel
+    {
+        public IEnumerable<string>? Locations { get; set; }
+
+        public void OnGet()
+        {
+            ViewData["Title"] = "SWC Bank - Locations";
+
+            Locations = new[]
+            {
+                "Albuquerque Branch,\n" +
+                "    86 Cromwell Path\n" +
+                "Albuquerque, NM 75309",
+            };
+        }
+    }
+}
