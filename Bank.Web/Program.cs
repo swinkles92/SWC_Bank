330cbe3ef5dc68ce5fb52a118e6aecd54d6d4680
Sam Winkles <swinkles92@live.com>
2023-07-20 14:27:19 -0600

    Initial setup of webpage
    Created startup page for website. Created Pages, wwwroot folders and the files therein. Modified Bank.Web.csproj to enable duplicate items (to bypass an error).

--- Bank.Web/Program.cs
+++ Bank.Web/Program.cs
@@ -1,7 +1,18 @@
 ﻿var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddRazorPages();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// The following enables HSTS and HTTPS redirection for security purposes
+if(!app.Environment.IsDevelopment())
+{
+    app.UseHsts();
+}
+app.UseHttpsRedirection();
+app.UseDefaultFiles();
+app.UseStaticFiles();
+app.MapRazorPages();
+app.MapGet("/hello", () => "Hello World!");
 
 app.Run();
 
+WriteLine("Web server has stopped.");
