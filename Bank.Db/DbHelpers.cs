ea6a401da2703fd340815e51bdff3f2eedfd8aa7
Sam Winkles <swinkles92@live.com>
2023-07-25 14:41:06 -0600

    Created Bank.Db and DbTests Projects
    Created aforementioned projects. Created CreateConnection() method and various unit tests for it. Also created an empty ReadData() method.

---
+++ Bank.Db/DbHelpers.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Data.Sqlite;
+namespace Bank;
+
+public class DbHelpers
+{
+	/// <summary>
+	/// This method creates an SqliteConnection object and opens it with
+	/// either the default, or a provided, path.
+	/// </summary>
+	/// <param name="optionalPath">File path name for the database</param>
+	/// <returns>Opened SqliteConnection Object</returns>
+	public SqliteConnection CreateConnection(string optionalPath = "" +
+            "Data Source = " +
+            "/Users/samuelwinkles/Documents/CSharpPrograms/swcbank.db;")
+	{
+		SqliteConnection sqlite_conn = new();
+		sqlite_conn.ConnectionString = optionalPath;
+		sqlite_conn.Open();
+
+		return sqlite_conn;
+	}
+	public void ReadData()
+	{
+
+	}
+}
+
---
+++ DbTests/DbTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Data.Sqlite;
+namespace Bank;
+
+public class DbTests
+{
+    /*
+     * CreateConnection() method tests
+     */
+
+    // Ensures that the CreateConnection() method in
+    // the DbHelpers file (in the Bank.Db project) correctly creates
+    // an SqliteConnection object
+    [Fact]
+    public void CreateConnectionTypeTest()
+    {
+        DbHelpers dbHelpers = new DbHelpers();
+        var sqlite_conn = dbHelpers.CreateConnection();
+        Assert.True(sqlite_conn is SqliteConnection);
+    }
+    // Ensures that the aforementioned CreateConnection() method
+    // results in an SqliteConnection with an Open state
+    [Fact]
+    public void CreateConnectionOpenTest()
+    {
+        DbHelpers dbHelpers = new DbHelpers();
+        var sqlite_conn = dbHelpers.CreateConnection();
+        Assert.True(sqlite_conn.State == System.Data.ConnectionState.Open);
+    }
+    // Ensures that the CreateConnection() method will be correctly assigned
+    // the optional path parameter if no other path parameter has been given
+    [Fact]
+    public void CreateConnectionDefaultConnectionStringTest()
+    {
+        DbHelpers dbHelpers = new DbHelpers();
+        var sqlite_conn = dbHelpers.CreateConnection();
+        Assert.True(sqlite_conn.ConnectionString.Equals("" +
+            "Data Source = " +
+            "/Users/samuelwinkles/Documents/CSharpPrograms/swcbank.db;"));
+    }
+}
